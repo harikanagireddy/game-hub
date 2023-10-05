9f6e002d04c3be5795beb6ae204e0aa342d5d069
nharika414 <68435242+nharika414@users.noreply.github.com>
2023-10-05 17:47:37 -0500

    Added Grid and Griditems

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
-import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav"" main"`,
+        lg: `"nav nav""aside main"`,
+      }}
+    >
+      <GridItem area="nav" bgColor="blue">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bgColor="violet">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bgColor="red">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
