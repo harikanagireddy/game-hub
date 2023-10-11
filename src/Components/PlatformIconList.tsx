3fdb1c1f132f50eecdf41a189b9ff99011d10593
nharika414 <68435242+nharika414@users.noreply.github.com>
2023-10-10 22:01:21 -0500

    Displating text with icons and solving index signature

--- src/Components/PlatformIconList.tsx
+++ src/Components/PlatformIconList.tsx
@@ -1,17 +1,41 @@
-import React from "react";
-import { Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaLinux,
+  FaAndroid,
+  FaApple,
+  FaPlaystation,
+  FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Icon, HStack } from "@chakra-ui/react";
 import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
 
 interface PlatformIconList {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: PlatformIconList) => {
+  const iconMap: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    android: FaAndroid,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    nintendo: SiNintendo,
+    web: BsGlobe,
+  };
   return (
     <>
-      {platforms.map((platform) => (
-        <Text>{platform.name}</Text>
-      ))}
+      <HStack marginY={1}>
+        {platforms.map((platform) => (
+          <Icon as={iconMap[platform.slug]} color="gray.500" />
+        ))}
+      </HStack>
     </>
   );
 };
